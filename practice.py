5abaecba94086075d6f441a05c4ec6afac4eca99
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-09-10 19:47:21 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,25 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+xpoints = np.array([0,5])
+ypoints = np.array([0,250])
+plt.plot(xpoints , ypoints)
+plt.show()
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+xpoints = np.array([1,2,6,8])
+ypoints = np.array([3,8,1,10])
+plt.plot(xpoints , ypoints)
+plt.show()
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+xpoints = np.array([1,2,6,8])
+ypoints = np.array([3,8,1,10])
+plt.plot(xpoints , ypoints,marker=0)
+plt.show()
